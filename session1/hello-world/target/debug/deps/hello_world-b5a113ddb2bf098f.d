64b8d281c616b531ed42e624da568f5f3ce0147e
JonAxtell-CT <122786953+JonAxtell-CT@users.noreply.github.com>
2023-01-25 11:36:22 +0000

    Delete session1/hello-world/target directory

--- session1/hello-world/target/debug/deps/hello_world-b5a113ddb2bf098f.d
+++
@@ -1,5 +0,0 @@
-/home/jonaxtell/Rust-Training/session1/hello-world/target/debug/deps/hello_world-b5a113ddb2bf098f: src/main.rs
-
-/home/jonaxtell/Rust-Training/session1/hello-world/target/debug/deps/hello_world-b5a113ddb2bf098f.d: src/main.rs
-
-src/main.rs:
--- session1/hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/home/jonaxtell/Rust-Training/session1/hello-world/target/debug/hello-world: /home/jonaxtell/Rust-Training/session1/hello-world/src/main.rs
