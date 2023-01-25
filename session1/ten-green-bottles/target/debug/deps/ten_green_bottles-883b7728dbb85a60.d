3291629d431e819f5f8f2ed3b24661b0116a9b96
JonAxtell-CT <122786953+JonAxtell-CT@users.noreply.github.com>
2023-01-25 11:36:38 +0000

    Delete session1/ten-green-bottles/target directory

--- session1/ten-green-bottles/target/debug/deps/ten_green_bottles-883b7728dbb85a60.d
+++
@@ -1,5 +0,0 @@
-/home/jonaxtell/Rust-Training/session1/ten-green-bottles/target/debug/deps/ten_green_bottles-883b7728dbb85a60: src/main.rs
-
-/home/jonaxtell/Rust-Training/session1/ten-green-bottles/target/debug/deps/ten_green_bottles-883b7728dbb85a60.d: src/main.rs
-
-src/main.rs:
--- session1/ten-green-bottles/target/debug/ten-green-bottles.d
+++
@@ -1,1 +0,0 @@
-/home/jonaxtell/Rust-Training/session1/ten-green-bottles/target/debug/ten-green-bottles: /home/jonaxtell/Rust-Training/session1/ten-green-bottles/src/main.rs
