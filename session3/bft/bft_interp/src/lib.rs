4dde1a3a19bec4030bbb6073f40e80d9df207f54
Jon Axtell <jon.axtell@codethink.co.uk>
2023-02-22 15:27:11 +0000

    Task two, start of interpreter

--- session3/bft/bft_interp/src/lib.rs
+++ session3/bft/bft_interp/src/lib.rs
@@ -1,7 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct BfTape<T> {
+    cell: Vec<T>,
+    grow: bool,
 }
 
+impl<T> BfTape<T> {
+    // Standard method of new
+    // pub fn new(&mut self, size: usize) -> &BfTape<T> {
+    //     if size == 0 {
+    //         self.cell = Vec::<T>::with_capacity(30000);
+    //     } else {
+    //         self.cell = Vec::<T>::with_capacity(size);
+    //     }
+    //     return self;
+    // }
+
+    // Spicy method of new
+    pub fn new(&mut self, size: Option<core::num::NonZeroUsize>, grow: bool) -> &BfTape<T> {
+        self.grow = grow;
+        match size {
+            None => self.cell = Vec::<T>::with_capacity(30000),
+            Some(s) => self.cell = Vec::<T>::with_capacity(s.into()),
+        }
+        return self;
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
